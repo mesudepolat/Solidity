b58b99978c634e43f39d6c7af15dbcb76d29f8d2
Mesude <61362079+mesudepolat@users.noreply.github.com>
2023-03-17 11:56:30 +0300

    Add files via upload

---
+++ Constructor.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;	
+		
+// Creating a contract
+contract constructorExp {	
+		
+	// Declaring state variable
+	string community;	
+			
+	// Creating a constructor to set value of 'community'
+	constructor(string memory name)  {				
+		community = name;	
+	}	
+	
+	// Defining function to return the value of 'community'
+	function getValue() public view returns (string memory) {	
+		return community;	
+	}	
+}
